8aba24aa0849ddaf0b055c5171cdf6454881e608
normombre <113270044+normombre@users.noreply.github.com>
2023-03-02 17:38:27 +0900

    Add files via upload

--- test_4_1.py
+++ test_4_1.py
@@ -2,35 +2,7 @@ from collections import Counter
 import math
 import numpy as np
 
-columns = ['する:動詞:自立', 'いる:動詞:非自立', 'の:名詞:非自立', 
-           '事:名詞:非自立',
-           '云う:動詞:自立',
-           '老婆:名詞:一般',
-           'よう:名詞:非自立',
-           'なる:動詞:自立']
-
-def count(n, w):
-    if w in hgram[n]:
-        return hgram[n][w]
-    else:
-        return 0
-    
-    
-
-body = []
-for n in range(20):
-    temp = [count(n, w) for w in columns]
-    temp.append('芥川')
-    body.append(temp)
-for n in range(20):
-    temp = [count(n+20, w) for w in columns]
-    temp.append('菊池')
-    body.append(temp)
-
 path = "OTSML/"
-
-
-
 akutagawa = ["rashomon.txt","yoba.txt","yabuno_naka.txt","mujina.txt","hana.txt",
             "haguruma.txt","torokko.txt","toshishun.txt","shunkan.txt","shujuno_kotoba.txt",
             "jashumon.txt","shogun.txt","shigo.txt","agunino_kami.txt","aruhino_oishi_kuranosuke.txt",
@@ -41,25 +13,47 @@ kikuchi = ["anegawano_kassen.txt","aru_koino_hanashi.txt","irefuda.txt","emukosh
           "shobugoto.txt","dairiki_monogatari.txt","tojurono_koi.txt","aru_kogisho.txt",
           "minage_kyujogyo.txt","mumeisakkano_nikki.txt","wakasugi_saibancho.txt","bugyoto_ninsogaku.txt"]
 
-    
 hgram = []
 
 for ds in akutagawa:
-    counter = Counter(open(path+ds+"-decomp.txt", "r").read().split(" "))
+    counter = Counter(open(path+ds+"-decomp.txt", "r").read().split(" ")) # split(" ")ではスペースごとに区切ってリストを作成している
     hgram.append(dict(counter.most_common()))
 
 for ds in kikuchi:
     counter = Counter(open(path+ds+"-decomp.txt", "r").read().split(" "))
     hgram.append(dict(counter.most_common()))
 
- # 40編全ての名詞・形容詞・形容動詞・副詞の種類の総数
+def dl2(dictx, dicty):
+    sq = 0
+    for key in set(dictx.keys()) | set(dicty.keys()):
+        x, y = 0, 0
+        if key in dictx: 
+            x = dictx[key]
+        if key in dicty:
+            y = dicty[key]
+        sq += (x - y)**2
+    return math.sqrt(sq)
+
+temp = [] 
+for dictx in hgram:
+    temp.append(list(map(lambda dicty: dl2(dictx, dicty), hgram)))
+dl2_matrix = np.matrix(temp)
+
+
+akutagawa_name = ['羅生門', '妖婆', '藪の中', '貉', '鼻', '歯車', 'トロッコ', '杜子春', '俊寛', '侏儒の言葉', '邪宗門', '将軍', '死後', 'アグニの神', '或る日の大石内蔵助', 'おぎん', 'お時儀', '河童', '煙管', '蜘蛛の糸']
+kikuchi_name = ['姉川の戦い', 'ある恋の話', '入れ札', 'M公爵と写真師', '屋上の狂人', '恩讐の彼方に', '女強盗', '恩を返す話', '義民甚平', '勲章を貰う話', '極楽', '出世', '勝負事', '大力の物語', '藤十郎の恋', 'ある抗議書', '身投げ救助業', '無名作家の日記', '若杉裁判長', '奉行と人相学']
+
+name = akutagawa_name + kikuchi_name # 40作品すべてのタイトルのリスト生成
+
 union = set()
-for d in hgram:  # hgramは作品事(リストの引数に一つの作品が対応)に単語（形容詞・動詞・名詞・副詞）について、その出現頻度をカウントしたデータが入っている。
+for d in hgram:
     union |= d.keys()
-def count(dict, word): # dict内にwordが含まれているなら、その数を返す関数
-    if word in dict:
-        return dict[word]
+
+words = list(union)
+
+def count(n, w):
+    if w in hgram[n]:
+        return hgram[n][w]
     else:
         return 0
 raw_vector = np.array([[count(h, w) for w in union] for h in hgram]) # raw_vector[i][j]は、作品番号iの小説の中に単語番号jの単語が出現する回数
-len(raw_vector[0])
